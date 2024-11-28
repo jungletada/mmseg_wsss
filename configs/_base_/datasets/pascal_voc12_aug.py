aa3f45277bea66d138344cc048459503001c9f60
jungletada <1002955176@qq.com>
2024-11-28 16:15:43 +0800

    test

--- configs/_base_/datasets/pascal_voc12_aug.py
+++ configs/_base_/datasets/pascal_voc12_aug.py
@@ -26,8 +26,8 @@ test_pipeline = [
     dict(type='PackSegInputs')
 ]
 
-# img_ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
-img_ratios = [1.0]
+img_ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
+# img_ratios = [1.0]
 tta_pipeline = [
     dict(type='LoadImageFromFile', backend_args=None),
     dict(
--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -4,9 +4,9 @@ PORT=${PORT:-29500}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 CONFIG=configs/deeplabv3plus/deeplabv3plus_r101-d8_80k_mscoco-512x512.py
-# configs/deeplabv3plus/deeplabv3plus_r101-d8_4xb4-20k_voc12aug-512x512.py
+# configs/deeplabv3plus/deeplabv3plus_r101-d8_4xb4-40k_voc12aug-512x512.py
 CHECKPOINT=work_dirs/deeplabv3plus_r101-d8_80k_mscoco-512x512/iter_80000.pth
-# checkpoints/deeplabv3plus_r101-d8_512x512_20k_voc12aug_20200617_102345-c7ff3d56.pth
+# checkpoints/deeplabv3plus_r101-d8_512x512_40k_voc12aug_20200617_102345-c7ff3d56.pth
 GPUS=2
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
